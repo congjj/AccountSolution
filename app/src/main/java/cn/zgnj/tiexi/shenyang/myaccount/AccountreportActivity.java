dcf995ffc3069e581ef5020a908c6f8ff57f8613
congjj <congjiajia_007@hotmail.com>
2017-10-06 10:13:20 +0800

    创建记账表-对账UI和报表UI

---
+++ app/src/main/java/cn/zgnj/tiexi/shenyang/myaccount/AccountreportActivity.java
@@ -0,0 +1,15 @@
+package cn.zgnj.tiexi.shenyang.myaccount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountreportActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accountreport);
+    }
+}
