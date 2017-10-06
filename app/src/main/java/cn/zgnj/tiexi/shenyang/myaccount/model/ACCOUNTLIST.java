c0546b85ca879a4e2d9d2435a6d11862985dfda5
congjj <congjiajia_007@hotmail.com>
2017-10-06 10:01:12 +0800

    创建记账表

---
+++ app/src/main/java/cn/zgnj/tiexi/shenyang/myaccount/model/ACCOUNTLIST.java
@@ -0,0 +1,58 @@
+package cn.zgnj.tiexi.shenyang.myaccount.model;
+
+import com.orm.SugarRecord;
+import com.orm.dsl.Column;
+
+import java.io.Serializable;
+
+import cn.zgnj.tiexi.shenyang.myaccount.IModelHelper;
+
+/**
+ * Created by Administrator on 2017/10/6.
+ */
+
+public class ACCOUNTLIST extends SugarRecord implements Serializable,IModelHelper
+{
+    /**
+     * 外键 记账项目 ID
+     */
+    @Column(name = "ACCOUNTSUBJECT_ID",notNull = true)
+    ACCOUNTSUBJECT  mACCOUNTSUBJECT ;
+
+    /**
+     * 花销名称
+     */
+    @Column(name = "NAME",notNull = true)
+    String mNAME;
+
+    /**
+     * 花销数量
+     */
+    @Column(name = "COUNT",notNull = true)
+    Double mCOUNT;
+
+    /**
+     * 单价
+     */
+    @Column(name = "PRICE",notNull = true)
+    Double mPRICE;
+
+    /**
+     * 是否已对账
+     */
+    @Column(name = "ISCHECKED",notNull = true)
+    Double mISCHECKED;
+
+    /**
+     * 备注
+     */
+    @Column(name = "REMARK")
+    String mREMARK;
+
+
+    @Override
+    public long _Insert()
+    {
+        return 0;
+    }
+}
