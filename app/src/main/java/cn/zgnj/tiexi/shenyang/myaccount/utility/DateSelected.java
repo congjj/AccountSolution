f694b470408219150373259222e186172b76251c
congjj <congjiajia_007@hotmail.com>
2017-10-09 22:40:59 +0800

    鲅鱼圈最后更新22:40

--- app/src/main/java/cn/zgnj/tiexi/shenyang/myaccount/utility/DateSelected.java
+++ app/src/main/java/cn/zgnj/tiexi/shenyang/myaccount/utility/DateSelected.java
@@ -1,12 +1,24 @@
 package cn.zgnj.tiexi.shenyang.myaccount.utility;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import cn.zgnj.tiexi.shenyang.myaccount.R;
 
@@ -15,32 +27,16 @@ import cn.zgnj.tiexi.shenyang.myaccount.R;
  * Created by CJJ on 2017/10/9.
  */
 
-public class DateSelected extends View
+public class DateSelected extends LinearLayout
 {
     private TextView mTvTitle;
     private TextView mTvDateValue;
     private Button mBtnSelect;
-    private Context _context;
 
 
-    public DateSelected (Context context)
-    {
-        this(context,null,0);
-    }
 
-    public DateSelected (Context context, AttributeSet attrs)
-    {
-        this(context, attrs, 0);
-    }
 
-    public DateSelected(Context context, AttributeSet attrs, int defStyle)
-    {
-        super(context, attrs, defStyle);
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_dateselected ,null,true);
-        mBtnSelect =(Button)view.findViewById(R.id .btnSelectDate) ;
-        mTvTitle =(TextView )view.findViewById(R.id .tvTitile);
-        mTvDateValue =(TextView)view.findViewById(R.id .tvDateValue) ;
-    }
+    private static  Context mContext ;
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
@@ -53,6 +49,50 @@ public class DateSelected extends View
     {
 
     }
+
+    /**
+     * 获取本机时间
+     * @return
+     */
+    public Date getNow()
+    {
+        return new Date() ;
+    }
+
+    /**
+     * 设置标题
+     * @param text
+     */
+    public void setTitleText(String text)
+    {
+        mTvTitle.setText(text) ;
+    }
+
+    public DateSelected (Context context)
+    {
+        this(context,null,0);
+    }
+
+    public DateSelected (Context context, AttributeSet attrs)
+    {
+        this(context, attrs, 0);
+    }
+
+    public DateSelected(final Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_dateselected ,this ,true);
+        mBtnSelect =(Button)view.findViewById(R.id .btnSelectDate) ;
+        mTvTitle =(TextView )view.findViewById(R.id .tvTitile);
+        mTvDateValue =(TextView)view.findViewById(R.id .tvDateValue) ;
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        mTvDateValue .setText(df.format(getNow())) ;
+
+    }
+
+
+
 
 
 
